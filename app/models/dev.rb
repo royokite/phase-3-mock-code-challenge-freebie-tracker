72418eda1040c9c6bf3ad8dc6d1ecb93190b48a9
RoyOkite <royokite@gmail.com>
2022-11-09 01:07:31 +0300

    Completed Freebie Tracker Mock Challenge

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,18 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        if self.freebies.find_by(item_name: item_name)
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+        if self.id == freebie.dev_id
+            freebie.update(dev_id: dev.id)
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+company1 = Company.create(name: "Google", founding_year: 1998)
+company2 = Company.create(name: "Facebook", founding_year: 2004)
+company3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+company4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+dev1 = Dev.create(name: "Rick")
+dev2 = Dev.create(name: "Morty")
+dev3 = Dev.create(name: "Mr. Meseeks")
+dev4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "Baseball Cap", value: 250, company_id: company1.id, dev_id: dev1.id)
+Freebie.create(item_name: "Laptop Skin", value: 3200, company_id: company2.id, dev_id: dev2.id)
+Freebie.create(item_name: "Phone Cover", value: 1500, company_id: company3.id, dev_id: company3.id)
+Freebie.create(item_name: "Mug", value: 100, company_id: company4.id, dev_id: dev4.id)
 
 puts "Seeding done!"
